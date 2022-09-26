a2e9375a748e4bb5b98f780a09b3f2df6f72bc4f
Aavin Fernando <aavin.fernando@gmail.com>
2022-09-26 18:55:15 +1000

    [UI] Basic parts of UI

---
+++ Assets/Scripts/UI/OptionsBackend.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenuBackend : MonoBehaviour
+{
+    void Awake() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
