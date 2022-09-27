7ec98d1de1737cd6aaa6ff906816195c4b736020
Julian <progamerguy005@gmail.com>
2022-09-27 17:13:50 +1000

    started attempt at creating UI for fractured memroies

--- Assets/Scripts/FracturedMemories/FracturedMemoryManager.cs
+++ Assets/Scripts/FracturedMemories/FracturedMemoryManager.cs
@@ -3,19 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using Newtonsoft.Json;
+using UnityEngine.UIElements;
+using System;
 
 
 public class FracturedMemoryManager : MonoBehaviour
 {
     private string m_path;
     private Memory[] Memories;
+    private int m_nextID;
+
+    private List<string> m_storyList = new List<string>();
+    private List<string> m_nameList = new List<string>();
+    
+    public ListView displayNameList;
+    public ScrollView displaystoryList;
 
     // Start is called before the first frame update
     void Start()
     {
         //m_path = wherever tf i need to put it in the game files
 
+        m_nextID = 0;
         Memories = GenerateMemories(m_path);
+    }
+
+    void OnEnable()
+    {
+        Func<VisualElement> makeName = () => new Label();
+
+        Action<VisualElement, int> bindName = (e, i) => (e as Label).text = m_nameList[i];
+
+        // const int itemHeight = 16;
+
+        displayNameList.selectionType = SelectionType.Multiple;
+
+        displayNameList.onItemsChosen += objects => Debug.Log(objects);
+        displayNameList.onSelectionChange += objects => Debug.Log(objects);
     }
 
     // Update is called once per frame
@@ -43,6 +67,16 @@ public class FracturedMemoryManager : MonoBehaviour
 
     void AddMemory(string name) 
     {
-
+        foreach (Memory m in Memories) 
+        {
+            if (m.name == name) 
+            {
+                m.obtained = true;
+                m.ID = m_nextID;
+                m_nextID += 1;
+                m_storyList.Add(m.story);
+                m_nameList.Add(m.name);
+            }
+        }
     }
 }
