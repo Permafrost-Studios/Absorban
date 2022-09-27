f4b0f0a260e8ab1c6e03cd197db04aeae604c117
Aavin Fernando <aavin.fernando@gmail.com>
2022-09-27 18:48:00 +1000

    [UI] Fix Border :( why .unitybutton 😭

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System;
 
 public class OptionsMenu : MonoBehaviour
 {
+    private List<VisualElement> items;
+    [SerializeField] private VisualTreeAsset m_OptionEntryTemplate;
     [SerializeField] private UIDocument m_UIDocument;
 
     #nullable enable
     private GameObject? m_returnTo;
-
     public void SetReturnToObject(GameObject returnto) => m_returnTo = returnto;
 
-    // Get UI Document and register callbacks
-    void Start() {
+    // Get UI Document and register callbacks, MUST be OnEnable
+    void OnEnable() {
         VisualElement rootVis =  m_UIDocument.rootVisualElement;
         Button quit = rootVis.Q<Button>("Return");
         quit.clicked += OnReturnClicked;
+        PopulateListView(rootVis);
     }
     
     void OnReturnClicked() {
@@ -29,7 +32,44 @@ public class OptionsMenu : MonoBehaviour
     #nullable restore
 
 
-    void Update() {
+    void PopulateListView(VisualElement rootVis) {
+        Debug.Log("Attempt PopulateListView");
+        ListView optlist = rootVis.Q<ListView>("OptionsList");
+        
+        items = new List<VisualElement>();
+
+        optlist.bindItem = OnBindItem;
+        optlist.makeItem = OnMakeItem;
+        optlist.itemsSource = items;
+        optlist.fixedItemHeight = 80;
+
+        optlist.onItemsChosen += objects => Debug.Log(objects);
+
+
+        var temp = new MinMaxSlider(0f,100f,0f,100f);
+        var temp2 = new MinMaxSlider(1f,100f,0f,100f);
+        items.Add(temp);
+        items.Add(temp2);
+
 
     }
+
+    void OnBindItem(VisualElement elem, int idx) {
+        ElemData elm = (elem as ElemData);
+        elm._lbl.text = "hello";
+        // elem.user 
+        // (elem as Label).text = "hello";
+    }
+
+    VisualElement OnMakeItem() {
+        var newListEntry = m_OptionEntryTemplate.Instantiate();
+        // newListEntry.
+        return newListEntry;
+    }
+
+    class ElemData : VisualElement {
+        public Label _lbl;
+        public VisualElement _elem;
+    }
+
 }
