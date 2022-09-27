c86211e3e25b72a5eb8e8fa8cd2810f8be65743d
Aavin Fernando <aavin.fernando@gmail.com>
2022-09-27 14:10:51 +1000

    [UI] Some backend

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private UIDocument m_UIDocument;
+    [SerializeField] private GameObject m_optionsMenu;
+
+    // Get UI Document and register callbacks, MUST be OnEnable
+    void OnEnable() {
+        VisualElement rootVis =  m_UIDocument.rootVisualElement;
+
+        Button strt = rootVis.Q<Button>("Start");
+        Button optn = rootVis.Q<Button>("Options");
+        Button crdt = rootVis.Q<Button>("Credits");
+        Button quit = rootVis.Q<Button>("Quit");
+
+
+        strt.clicked += OnStartClicked;
+        optn.clicked += OnOptionsClicked;
+        crdt.clicked += OnCreditsClicked;
+        quit.clicked += OnQuitClicked;
+    }
+
+    void OnStartClicked() {
+        Debug.Log("start button clicked");
+    }
+
+    void OnOptionsClicked() {
+        Debug.Log("options button clicked");
+        Instantiate(m_optionsMenu).GetComponent<OptionsMenu>().SetReturnToObject(this.gameObject);
+        this.gameObject.SetActive(false);
+    }
+
+    void OnCreditsClicked() {
+        Debug.Log("credits button clicked");
+    }
+
+    void OnQuitClicked() {
+        Application.Quit();
+    }
+}
