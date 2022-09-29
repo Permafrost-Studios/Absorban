5e3d18caf39ee592405d572dced634ef790db8fc
Aavin Fernando <aavin.fernando@gmail.com>
2022-09-29 18:26:31 +1000

    [Weapons] Add sample weapon and change weapon firing to semi/auto

--- Assets/Scripts/Weapons/WeaponProjectile.cs
+++ Assets/Scripts/Weapons/WeaponProjectile.cs
@@ -8,18 +8,22 @@ public class WeaponProjectile : MonoBehaviour
     private float m_damage;
     private Rigidbody2D body;
 
-    public void Shoot(float damage, float shootForce, float directionMultiplier) 
-    {
+    public void Shoot(float damage, float shootForce, float directionMultiplier) {
         m_damage = damage;
         body = GetComponent<Rigidbody2D>();
         body.AddForce(transform.right * shootForce * directionMultiplier);
     }
 
+    public void Shoot(float damage, float shootForce, Vector2 direction) {
+        m_damage = damage;
+        body = GetComponent<Rigidbody2D>();
+        body.AddForce(direction.normalized * shootForce);
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log(m_damage);
-        if (other.gameObject.layer == 8)
-        {
+        if (other.gameObject.layer == 8) {
             other.gameObject.GetComponent<EnemyHealth>().TakeDamage(m_damage);
         } 
         Destroy(gameObject);
--- Assets/Scripts/Weapons/WeaponShoot.cs
+++ Assets/Scripts/Weapons/WeaponShoot.cs
@@ -12,21 +12,34 @@ public class WeaponShoot : MonoBehaviour
 
     private float m_remainingCooldown;
 
+    private PlayerMoving m_moving; 
+
+    // GetButtonDown or GetButton, false = semi
+    [SerializeField] private bool semiorautomatic;
+
+
+    // To spawn the projectile at the end of the weapon
+    [SerializeField] private float weaponlength;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        m_remainingCooldown = shootCooldown;        
+    void Start() {
+        m_remainingCooldown = shootCooldown;
+        m_moving = this.gameObject.transform.parent.parent.gameObject.GetComponent<PlayerMoving>();
+        weaponlength = this.GetComponent<SpriteRenderer>().bounds.extents.x;      
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         m_remainingCooldown -= Time.deltaTime;
 
-        if(Input.GetButtonDown("Fire1")) 
-        {
-            GameObject current = Instantiate(projectile);
-            float directionMultiplier = gameObject.transform.parent.parent.gameObject.GetComponent<PlayerMoving>().returnDirectionMultiplier();
+        // Debug.Log();
+
+        if(semiorautomatic ? Input.GetButton("Fire1") : Input.GetButtonDown("Fire1")) {
+            // Spawn bullet at the tip of the weapon
+            var tfm = this.transform.position + new Vector3(weaponlength,0f,-10f);
+
+            GameObject current = Instantiate(projectile, tfm, Quaternion.identity);
+            float directionMultiplier = m_moving.returnDirectionMultiplier();
             current.GetComponent<WeaponProjectile>().Shoot(weaponDamage, shootForce, directionMultiplier);
 
             m_remainingCooldown = shootCooldown;
