b71dcb1685abf941dc529a07604fe2bc09be21c4
Aavin Fernando <aavin.fernando@gmail.com>
2022-09-30 22:18:35 +1000

    [Weapons+Map8]

--- Assets/Scripts/Weapons/WeaponManager.cs
+++ Assets/Scripts/Weapons/WeaponManager.cs
@@ -31,6 +31,11 @@ public class WeaponManager : MonoBehaviour
     // Start is called before the first frame update
 
     void Start() {
+        if (!m_cam) {
+            m_cam = FindObjectOfType<Camera>();
+        }
+        m_moving = this.gameObject.transform.parent.gameObject.GetComponent<PlayerMoving>();
+
         // All in child-indexes, so 0-9
         currentActive = 0;
         discoveredWeapons =  new List<GameObject>();
@@ -38,6 +43,24 @@ public class WeaponManager : MonoBehaviour
 
         // Stub();
     }
+
+    [SerializeField] Camera m_cam;
+    private PlayerMoving m_moving; 
+
+        // Update is called once per frame
+    void Update() {
+        Vector2 mousescreenpos = Mouse.current.position.ReadValue();
+        
+        Vector2 deltapos = (Vector2)this.transform.position-(Vector2)m_cam.ScreenToWorldPoint(mousescreenpos);
+
+        Vector3 eulerangles = new Vector3(0f,0f,
+            (Mathf.Atan2(deltapos.y,deltapos.x)
+            *Mathf.Rad2Deg) - (m_moving.facingRight ? 0f : 180f)
+            );
+
+        this.transform.rotation = Quaternion.Euler(eulerangles);
+    }
+
 
     void RegisterCallbacks() {
         for (int i = 0; i < m_actions.Length; i++) {
