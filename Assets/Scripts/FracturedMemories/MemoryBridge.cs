0c45946d5c3e20a3ad192e72290e04f9cd3af103
Julian <progamerguy005@gmail.com>
2022-10-01 16:59:06 +1000

    removed uneccesary memorybridge scrit changer

--- Assets/Scripts/FracturedMemories/MemoryBridge.cs
+++ Assets/Scripts/FracturedMemories/MemoryBridge.cs
@@ -15,4 +15,9 @@ public class MemoryBridge : MonoBehaviour
     {
         memoryManager.GetComponent<FracturedMemoryManager>().AddMemory(name);
     }
+
+    void SetMemoryManager(GameObject obj) 
+    {
+        //memoryManager =
+    }
 }
